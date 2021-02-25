603b8ef60be76bb52ad6c540fa9c3c3bea755744
stamander <iida.atsushi.fh@gmail.com>
2021-02-26 06:52:11 +0900

    修正

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,4 +2,12 @@ class ContactController < ApplicationController
 
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'front#index'
   resources :front, only: [:index,:show] 
   resources :introduction, only: [:index] 
-  resources :contact, only: [:index]
+  resources :contact
   resources :history, only: [:index]
 
 end
